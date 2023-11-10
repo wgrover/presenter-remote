f7be467d13dd61b89ecc22f6c0eff73e1eb442a4
William H. Grover <wgrover@gmail.com>
2023-11-10 10:51:51 -0800

    Blue onboard LED now shows if connected

--- presenter-remote/presenter-remote.ino
+++ presenter-remote/presenter-remote.ino
@@ -15,11 +15,13 @@ void setup() {
   esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_DEFAULT, ESP_PWR_LVL_P9); 
   pinMode(19, INPUT_PULLUP);  // backward button
   pinMode(21, INPUT_PULLUP);  // forward button
+  pinMode(2, OUTPUT);  // onboard blue LED
   keepAliveTime = millis() + 1000;
 }
 
 void loop() {
   if(bleKeyboard.isConnected()) {
+    digitalWrite(2, HIGH);
     currentState = digitalRead(21);
     if (currentState != lastFlickerableState) {
       lastDebounceTime = millis();
@@ -33,9 +35,17 @@ void loop() {
       lastSteadyState = currentState;
     }
     if (millis() > keepAliveTime) {
+      Serial.print(millis());
+      Serial.print(" ");
       Serial.println("keep alive");
       bleKeyboard.write(KEY_LEFT_SHIFT);
       keepAliveTime = millis()+1000;
     }
+  }
+  else {
+    digitalWrite(2, LOW);
+    Serial.print(millis());
+    Serial.print(" ");
+    Serial.println("not connected");
   }
 }
